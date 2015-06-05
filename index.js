ef622677581e212886d029c04c95b23c46f3d2b9
Martin Feckie <mfeckie@gmail.com>
2015-06-05 21:31:30 +1000

    Add opal javascript dependencies to build

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-opal-rb'
+  name: 'ember-cli-opal-rb',
+  included: function (app) {
+    this._super.included(app);
+
+    app.import('vendor/opal.min.js');
+    app.import('vendor/opal-parser.min.js');
+  }
 };
